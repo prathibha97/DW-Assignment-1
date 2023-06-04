43e33f98048d23def04a924463d5526b4eaad0ad
Prathibha Ratnayake <prsthibha@gmail.com>
2023-06-04 15:35:37 +0530

    Add HealthTips component

---
+++ src/components/HealthTip.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <h2 class="text-xl font-semibold mb-2">{{ title }}</h2>
+    <p class="text-gray-700">
+      {{ desc }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HealthTipComponent',
+  props: {
+    title: String,
+    desc: String
+  }
+}
+</script>
--- src/views/HealthTipsView.vue
+++ src/views/HealthTipsView.vue
@@ -10,58 +10,8 @@
     <div class="container mx-auto px-4 py-8 h-[80vh] mb-[520px] md:mb-0">
       <h1 class="text-3xl font-bold mb-4">Pet Health Tips</h1>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        <!-- Tip 1 -->
-        <div class="bg-white p-6 rounded-lg shadow-md">
-          <h2 class="text-xl font-semibold mb-2">Tip 1: Nutrition</h2>
-          <p class="text-gray-700">
-            Provide a balanced diet with high-quality pet food. Avoid feeding them harmful human
-            foods.
-          </p>
-        </div>
-
-        <!-- Tip 2 -->
-        <div class="bg-white p-6 rounded-lg shadow-md">
-          <h2 class="text-xl font-semibold mb-2">Tip 2: Regular Exercise</h2>
-          <p class="text-gray-700">
-            Engage your pet in daily physical activities to maintain their overall health and
-            fitness.
-          </p>
-        </div>
-
-        <!-- Tip 3 -->
-        <div class="bg-white p-6 rounded-lg shadow-md">
-          <h2 class="text-xl font-semibold mb-2">Tip 3: Vaccinations</h2>
-          <p class="text-gray-700">
-            Keep your pet's vaccinations up to date to prevent common diseases and ensure their
-            well-being.
-          </p>
-        </div>
-
-        <!-- Tip 4 -->
-        <div class="bg-white p-6 rounded-lg shadow-md">
-          <h2 class="text-xl font-semibold mb-2">Tip 4: Dental Care</h2>
-          <p class="text-gray-700">
-            Brush your pet's teeth regularly and provide dental treats to maintain their oral
-            hygiene.
-          </p>
-        </div>
-
-        <!-- Tip 5 -->
-        <div class="bg-white p-6 rounded-lg shadow-md">
-          <h2 class="text-xl font-semibold mb-2">Tip 5: Mental Stimulation</h2>
-          <p class="text-gray-700">
-            Engage your pet in interactive play and provide mental stimulation to prevent boredom
-            and anxiety.
-          </p>
-        </div>
-
-        <!-- Tip 6 -->
-        <div class="bg-white p-6 rounded-lg shadow-md">
-          <h2 class="text-xl font-semibold mb-2">Tip 6: Regular Vet Check-ups</h2>
-          <p class="text-gray-700">
-            Schedule routine check-ups with a veterinarian to monitor your pet's health and detect
-            any potential issues early.
-          </p>
+        <div v-for="tip in healthTips" :key="tip.title">
+          <HealthTip :title="tip.title" :desc="tip.desc" />
         </div>
       </div>
     </div>
@@ -69,12 +19,44 @@
 </template>
 
 <script>
+import HealthTip from '../components/HealthTip.vue'
 import SubHero from '../components/SubHero.vue'
 
 export default {
+  data() {
+    return {
+      healthTips: [
+        {
+          title: 'Tip 1: Nutrition',
+          desc: 'Engage your pet in interactive play and provide mental stimulation to prevent boredom and anxiety.'
+        },
+        {
+          title: 'Tip 2: Regular Exercise',
+          desc: 'Engage your pet in daily physical activities to maintain their overall health and fitness.'
+        },
+        {
+          title: 'Tip 3: Vaccinations',
+          desc: "Keep your pet's vaccinations up to date to prevent common diseases and ensure their well-being."
+        },
+        {
+          title: 'Tip 4: Dental Care',
+          desc: "Brush your pet's teeth regularly and provide dental treats to maintain their oral hygiene."
+        },
+        {
+          title: 'Tip 5: Mental Stimulation',
+          desc: 'Engage your pet in interactive play and provide mental stimulation to prevent boredom and anxiety.'
+        },
+        {
+          title: 'Tip 6: Regular Vet Check-ups',
+          desc: "Schedule routine check-ups with a veterinarian to monitor your pet's health and detect any potential issues early."
+        }
+      ]
+    }
+  },
   name: 'HealthTipsView',
   components: {
-    SubHero
+    SubHero,
+    HealthTip
   }
 }
 </script>
